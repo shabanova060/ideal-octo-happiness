646b21b0feb720d03c198ebeb37acfe1635d4021
shabanova060 <shabanova060@gmail.com>
2026-04-29 11:57:55 +0200

    Initial commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,14 @@
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/')({
+  component: HomePage,
+})
+
+function HomePage() {
+
+  return (
+    <main>
+      <h1>Home Page</h1>
+    </main>
+  )
+}
